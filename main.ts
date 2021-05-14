aec26968f2bb02140c2752b9a494fe44b4202c3f
Daniye32 <82909899+Daniye32@users.noreply.github.com>
2021-05-14 16:25:54 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,53 @@
+music.playMelody("C5 B A G F E D C ", 120)
+basic.showLeds(`
+    . # # # .
+    . # . . #
+    . # . . #
+    . # . . #
+    . # # # .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    . # # # .
+    . # . # .
+    . # # # .
+    . # . # .
+    . # . # .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # . . . #
+    # # . . #
+    # . # . #
+    # . . # #
+    # . . . #
+    `)
+basic.pause(100)
+basic.showLeds(`
+    . . # . .
+    . . . . .
+    . . # . .
+    . . # . .
+    . . # . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # .
+    # . . . .
+    # # # # .
+    # . . . .
+    # # # # .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # . . . .
+    # . . . .
+    # . . . .
+    # . . . .
+    # # # # .
+    `)
+basic.pause(100)
+music.playMelody("C D E F G A B C5 ", 120)
+basic.forever(function () {
+	
+})
